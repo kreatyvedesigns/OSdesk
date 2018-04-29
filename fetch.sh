ea4c91e323bd1df61c58573ca988c91d12b42496
GTOSP (Gleentech) <pedrolvicente@gmail.com>
2018-04-29 18:12:24 +0100

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -3,3 +3,5 @@
 sudo apt install docky tint2 build-essential openbox i3wm docker docker-compose lightdm cairo-dock  compton -y 
 
 sudo dpkg-reconfigure gdm3
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
