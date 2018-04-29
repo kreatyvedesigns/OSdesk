7c9b69074d3e63d12125f40c243cce87a3ca5c95
GTOSP (Gleentech) <pedrolvicente@gmail.com>
2018-04-29 16:20:21 +0100

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-sudo apt install docky tint2 build-essential openbox i3wm docker docker-compose lightdm -y 
+sudo apt install docky tint2 build-essential openbox i3wm docker docker-compose lightdm cairo-dock  compton -y 
